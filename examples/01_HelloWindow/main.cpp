43e4cdffce6f46f3c204c34575a0e3ebde2509b1
Suhan <suhanjin17@gmail.com>
2026-03-24 10:37:22 +0900

    locate to platform

--- examples/01_HelloWindow/main.cpp
+++ examples/01_HelloWindow/main.cpp
@@ -1,11 +1,13 @@
-﻿#include "Core/App/Window.h"
+﻿#include "Platform/Window.h"
 #include <iostream>
 #include <string.h>
+
+using namespace dy::Platform;
 
 int main()
 {
 	Window window(800,600);
-	while(!window.ShouldClose())
+	while(window.IsRunning())
 	{
 		window.PollEvents();
 	}
---
+++ src/Platform/Window.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+struct GLFWwindow;
+
+namespace dy::Platform
+{
+	class Window
+	{
+	public:
+		~Window();
+
+		Window(unsigned int width, unsigned int height);
+		Window(unsigned int width, unsigned int height, const char *title);
+
+		bool IsRunning() const;
+		void PollEvents() const;
+
+		void* GetHandle() const;
+
+	private:
+		struct GLFWwindow* m_window;
+	};
+}
